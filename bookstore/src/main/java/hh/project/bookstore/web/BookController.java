455ba4878d9ef335fe9113ff9ce5b18995471ea5
Pekka Levo <pekka.levo@myy.haaga-helia.fi>
2025-02-09 16:02:13 +0200

    Tehtävän 3.2  lisäys

--- bookstore/src/main/java/hh/project/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/project/bookstore/web/BookController.java
@@ -3,9 +3,18 @@ package hh.project.bookstore.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import hh.project.bookstore.repository.Repository;
+import hh.project.bookstore.domain.Book;
 
 @Controller
 public class BookController {
+
+    private final Repository repository;
+
+    public BookController(Repository repository) {
+        this.repository = repository;
+    }
+
 
     @GetMapping("/index")
     public String showIndex(Model model) {
@@ -13,4 +22,11 @@ public class BookController {
         return "index";
     }
 
+    @GetMapping("/booklist")
+    public String showBookList(Model model) {
+        Iterable<Book> books = repository.findAll();
+        model.addAttribute("books", books);
+        return "booklist";
+    }
 }
+
