b3194d514c0f335b46c2854b5b1576c8843244df
Pekka Levo <pekka.levo@myy.haaga-helia.fi>
2025-02-27 16:30:16 +0200

    Tehtävän 5.2 lisäys

--- bookstore/src/main/java/hh/project/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/hh/project/bookstore/repository/BookRepository.java
@@ -3,8 +3,10 @@ package hh.project.bookstore.repository;
 import hh.project.bookstore.domain.Book;
 import hh.project.bookstore.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
 
+@RepositoryRestResource(path = "books")
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByCategory(Category category); 
+    List<Book> findByCategory(Category category);
 }
--- bookstore/src/main/java/hh/project/bookstore/repository/CategoryRepository.java
+++ bookstore/src/main/java/hh/project/bookstore/repository/CategoryRepository.java
@@ -2,6 +2,8 @@ package hh.project.bookstore.repository;
 
 import hh.project.bookstore.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "categories")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
